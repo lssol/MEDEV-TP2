ac5d13fe9531b5d067aab3f508fc2235b202ae8f
Sacha Brisset <sacha.brisset@hotmail.fr>
2015-11-26 11:39:28 +0100

    Implémentation du constructeur de ModelePGM qui importe un fichier source PGM

--- tp2/ModelePGM.java
+++ tp2/ModelePGM.java
@@ -8,23 +8,28 @@ package tp2;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.StringTokenizer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
  * Represente le fichier PGM
+ *
  * @author seti
  */
 public class ModelePGM {
+
     private String source;
     private BufferedReader fichier;
     private int width;
     private int height;
     /**
-     * Désigne la matrice qui represente le fichier : chaque ligne represente une ligne réèlle de l'image
+     * Désigne la matrice qui represente le fichier : chaque ligne represente
+     * une ligne réèlle de l'image
      */
     private int[][] matrice;
+
 
     public int getWidth() {
         return width;
@@ -33,19 +38,70 @@ public class ModelePGM {
     public int getHeight() {
         return height;
     }
+
+    public int[][] getMatrice() {
+        return matrice;
+    }
     
+
     /**
      * Le constructeur de la classe réalise l'importation du fichier source
-     * @param source est le lien vers le fichier à importer 
+     * A savoir : une fois les lignes du début ecrite, les sauts de ligne ne sont plus d'aucune importance
+     *
+     * @param source est le lien vers le fichier à importer
      */
-    public ModelePGM(String source) {
-        
+    public ModelePGM(String source) throws IOException {
+        this.source = source;
+        try {
+            this.fichier = new BufferedReader(new FileReader(source));
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(ModelePGM.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        String delimiteur = " ";
+        String ligne;
+        // On se place à la ligne où on definit les dimensions
+        ligne = fichier.readLine();
+        ligne = fichier.readLine();
+        ligne = fichier.readLine();
+
+        //Initialisation de width
+        StringTokenizer tokenizer = new StringTokenizer(ligne, delimiteur);
+        width = Integer.parseInt(tokenizer.nextToken());
+        height = Integer.parseInt(tokenizer.nextToken());
+        ligne = fichier.readLine();
+        ligne = fichier.readLine();
+        matrice = new int[width][height];
+
+        // Compteur de mots, reviens à 0 quand on atteint width
+        int compteur = 0;
+        int i = 0; // ligne dans la matrice
+        int j = 0; // colonne dans la matrice
+
+        while (ligne != null) {
+            StringTokenizer tok = new StringTokenizer(ligne, delimiteur);
+
+            while (tok.hasMoreTokens()) {
+                // Lorsqu'on a finis une ligne
+                if (compteur == width) {
+                    compteur = 0;
+                    i++;
+                }
+                matrice[i][j] = Integer.parseInt(tok.nextToken());
+                j++;
+                compteur++;
+            }
+            // on passe à la ligne suivante du fichier
+            ligne = fichier.readLine();
+        }
+        fichier.close();
     }
+
     /**
      * Crée un fichier sous format PGM
      * @param nomFichier Le nom du fichier
      */
     public void ecrire(String nomFichier){
-        
+
     }
 }
