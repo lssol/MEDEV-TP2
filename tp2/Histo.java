d710a166f6c1b44d5a411cd8caf74075b27cb0d4
Sacha Brisset <sacha.brisset@hotmail.fr>
2015-11-26 15:19:11 +0100

    Corrections mineures (maintenant fonctionnel)

--- tp2/Histo.java
+++ tp2/Histo.java
@@ -4,66 +4,69 @@
 package tp2;
 
 /**
- * histogramme associé à une image PGM, c'est à dire représentation du nombre de pixels par teinte
- * 
+ * histogramme associé à une image PGM, c'est à dire représentation du nombre de
+ * pixels par teinte
+ *
  * @author oriane
  */
 public class Histo {
+
     /**
      * Vecteur contenant le nombre de pixels pour chaque teinte (entre 0 et 255)
      */
     private int valeurs[];
 
     /**
-     * Construit un objet Histo en fonction d'une matrice représentant une image en PGM
+     * Construit un objet Histo en fonction d'une matrice représentant une image
+     * en PGM
+     *
      * @param m modele de données associée à l'image source
      */
     public Histo(ModelePGM m) {
         // vnb de pixels pours les valeurs entre 0 et 255
         valeurs = new int[256];
-        
+
         int h = m.getHeight();
         int w = m.getWidth();
-        
+        int k;
         for (int i = 0; i < h; i++) {
             for (int j = 0; j < w; j++) {
-                int k = m.getMatrice()[i][j];
-                    valeurs[k] ++;
-                }
+                k = m.getMatrice()[i][j];
+                valeurs[k]++;
             }
-            
         }
-    
+
+    }
+
     /**
      * générère l'objet ModelePGM correspondant à l'histogramme courant
-     * 
+     *
      * @return image PGM représentant l'histogramme
      */
-    public ModelePGM genereModelePGM(){     
+    public ModelePGM genereModelePGM() {
         //hauteur de l'image PGM de l'histogramme
         int max = 0;
         for (int i = 0; i < valeurs.length; i++) {
             if (valeurs[i] > max) {
                 max = valeurs[i];
-            }          
-        }
-        
-        // création de la matrice des valeurs
-        int matrice[][] = new int[valeurs.length][max];
-        
-        for (int i = max-1 ; i>=0 ; i--){
-            for (int j = 0; j < valeurs.length; j++) {
-                if (valeurs[j] > 0){
-                    matrice[i][j] = 255;
-                    valeurs[j] --;
-                }       
             }
         }
-        
+
+        // création de la matrice des valeurs
+        int matrice[][] = new int[max][valeurs.length-1];
+
+        for (int i = max - 1; i >= 0; i--) {
+            for (int j = 0; j < valeurs.length; j++) {
+                if (valeurs[j] > 0) {
+                    matrice[i][j] = 255;
+                    valeurs[j]--;
+                }
+            }
+        }
+
         ModelePGM res = new ModelePGM(matrice);
-        
+
         return res;
     }
-    
-    
+
 }
